6a9a54b6379a54157071a2028cf2ab16f3a29144
Chiemelie <melikamchukwuemelie@gmail.com>
2025-10-12 02:10:04 +0100

    Update route.ts
    Reduced the number of questions fetched.

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -27,7 +27,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     const randomYear = searchParams.get('randomYear') ?? '';
 
     const englishResp: AxiosResponse<{ data: Question[] }> = await axios.get(
-      `https://questions.aloc.com.ng/api/v2/m/60`,
+      `https://questions.aloc.com.ng/api/v2/m/12`,
       {
         params: {
           subject: 'english',
@@ -49,7 +49,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
       subjects.map(async (subj): Promise<Fetched> => {
         try {
           const resp: AxiosResponse<{ data: Question[] }> = await axios.get(
-            `https://questions.aloc.com.ng/api/v2/q/40`,
+            `https://questions.aloc.com.ng/api/v2/q/8`,
             {
               params: { subject: subj },
               headers: {
